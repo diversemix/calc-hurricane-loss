54a26bfa2fa529b48b0849d149893e824941af6c
Peter Hooper <diversemix@gmail.com>
2020-01-19 14:51:29 +0000

    Revert previous changes to output number of events

--- loss_framework/calculator.py
+++ loss_framework/calculator.py
@@ -22,15 +22,13 @@ class Calculator:
 
         for year in range(n_years):
             year_loss = 0
-            t_events = 0
             for region in self.regions:
                 events = self.model.fn_event_model(region.event_rate)
-                t_events += events
                 loss = self.model.fn_loss_model(region.loss_mean, region.loss_stddev, events)
                 loss_total = sum(loss)
                 year_loss += loss_total
                 if isLogging:
                     self.log.debug("{:10}: Year={}, Events={}, Loss={}".format(region.name, year, events, loss_total))
-            result.add_annual_loss(year_loss, t_events)
+            result.add_annual_loss(year_loss)
 
         return result
--- loss_framework/loss_result.py
+++ loss_framework/loss_result.py
@@ -8,16 +8,13 @@ class LossResult:
     """
     num_years: int = 0
     total_loss: float = 0
-    events: int = 0
 
-    def add_annual_loss(self, loss: float, events: int) -> None:
+    def add_annual_loss(self, loss: float) -> None:
         self.num_years += 1
         self.total_loss += loss
-        self.events += events
 
     def __str__(self) -> str:
         if self.num_years:
-            # return "Mean loss = {:.3f} per year calculated over {} years.".format(self.total_loss / self.num_years, self.num_years)
-            return "{}, {}".format(self.total_loss / self.num_years, self.events/ self.num_years)
+            return "Mean loss = {:.3f} per year calculated over {} years.".format(self.total_loss / self.num_years, self.num_years)
         else:
             return "No result"
