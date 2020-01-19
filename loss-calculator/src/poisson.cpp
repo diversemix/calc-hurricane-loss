b03c4c90dd9bc5b5ace781c7c081691958df6315
Peter Hooper <diversemix@gmail.com>
2020-01-19 12:45:23 +0000

    speed up by moving generator into constructor

--- loss-calculator/src/poisson.cpp
+++ loss-calculator/src/poisson.cpp
@@ -8,20 +8,20 @@ class EventDefinition {
       this->event_rate = rate;
       this->loss_mean = mean;
       this->loss_stddev = stddev;
+
+      this->event_distribution = std::poisson_distribution<>(this->event_rate);
+      this->loss_distribution = std::lognormal_distribution<>(this->loss_mean, this->loss_stddev);
+      this->generator = std::mt19937(this->rd());
+
     }
 
     long double loss_in_year() {
-      std::mt19937 gen(this->rd());
-      // double gen = 1;
-
-      std::poisson_distribution<int> d_events(this->event_rate);
-      std::lognormal_distribution<> d_loss(this->loss_mean, this->loss_stddev);
 
       long double total = 0;
-      int num_events = d_events(gen);
+      int num_events = this->event_distribution(this->generator);
       for (int i = 0; i < num_events; i++)
       {
-        total += d_loss(gen); 
+        total += this->loss_distribution(this->generator); 
       }
 
       return total;
@@ -33,6 +33,11 @@ class EventDefinition {
     long double loss_stddev;
 
     std::random_device rd;
+
+      // https://en.cppreference.com/w/cpp/numeric/random/poisson_distribution/poisson_distribution
+    std::poisson_distribution<> event_distribution;
+    std::lognormal_distribution<> loss_distribution;
+    std::mt19937 generator;
 
 };
 
